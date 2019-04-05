df39fe6f4454c0c50ea3b08cae71b196b99049c3
Domenico Giambra <zorinik@gmail.com>
2019-04-05 15:58:54 +0200

    Other enhancements in transition from old version

--- Config.php
+++ Config.php
@@ -29,9 +29,9 @@ $config = [
 		if (file_exists(INCLUDE_PATH . 'app/config/Paypal/class.php')) { // Transition from old version
 			$code = file_get_contents(INCLUDE_PATH . 'app/config/Paypal/class.php');
 			$code = str_replace('class Paypal extends PaypalBase', 'class PaypalCheck extends PaypalCheckBase', $code);
-			$code = preg_replace('/function verifyOrder.+/', 'function verifyOrder\(string $id, float $tot\): int', $code);
-			$code = preg_replace('/function execute.+/', 'function execute\(array $response, string $type\)', $code);
-			$code = preg_replace('/function alreadyExecuted.+/', 'function alreadyExecuted\(array $response, string $type\)', $code);
+			$code = preg_replace('/function verifyOrder.+/', 'function verifyOrder(string $id, float $paid): int', $code);
+			$code = preg_replace('/function execute.+/', 'function execute(array $response, string $type)', $code);
+			$code = preg_replace('/function alreadyExecuted.+/', 'function alreadyExecuted(array $response, string $type)', $code);
 			file_put_contents(INCLUDE_PATH . 'app/config/Paypal/PaypalCheck.php', $code);
 			unlink(INCLUDE_PATH . 'app/config/Paypal/class.php');
 		} else {
