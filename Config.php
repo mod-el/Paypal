5b53fe6e24697fe7a97ffd130d813787f8cf467f
Domenico Giambra <zorinik@gmail.com>
2019-04-05 15:56:19 +0200

    Wrong path

--- Config.php
+++ Config.php
@@ -20,20 +20,20 @@ $config = [
 ';
 		});
 
-		if (!file_exists(INCLUDE_PATH . 'app/config/Paypal/PaypalCheck.php') or file_exists(INCLUDE_PATH . 'app/config/class.php'))
+		if (!file_exists(INCLUDE_PATH . 'app/config/Paypal/PaypalCheck.php') or file_exists(INCLUDE_PATH . 'app/config/Paypal/class.php'))
 			$this->makePaypalClassFile();
 	}
 
 	private function makePaypalClassFile()
 	{
-		if (file_exists(INCLUDE_PATH . 'app/config/class.php')) { // Transition from old version
-			$code = file_get_contents(INCLUDE_PATH . 'app/config/class.php');
+		if (file_exists(INCLUDE_PATH . 'app/config/Paypal/class.php')) { // Transition from old version
+			$code = file_get_contents(INCLUDE_PATH . 'app/config/Paypal/class.php');
 			$code = str_replace('class Paypal extends PaypalBase', 'class PaypalCheck extends PaypalCheckBase', $code);
 			$code = preg_replace('/function verifyOrder.+/', 'function verifyOrder\(string $id, float $tot\): int', $code);
 			$code = preg_replace('/function execute.+/', 'function execute\(array $response, string $type\)', $code);
 			$code = preg_replace('/function alreadyExecuted.+/', 'function alreadyExecuted\(array $response, string $type\)', $code);
 			file_put_contents(INCLUDE_PATH . 'app/config/Paypal/PaypalCheck.php', $code);
-			unlink(INCLUDE_PATH . 'app/config/class.php');
+			unlink(INCLUDE_PATH . 'app/config/Paypal/class.php');
 		} else {
 			copy(INCLUDE_PATH . 'model/Paypal/PaypalCheckSample.php', INCLUDE_PATH . 'app/config/Paypal/PaypalCheck.php');
 		}
